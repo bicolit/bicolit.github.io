bbce7093928831a583276e188c78b1b112a7b377
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2019-10-24 12:56:36 +0800

    Remove unused javascript

--- assets/js/faq.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll(".accordion a");
-
-function toggleAccordion(){
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('active');
-}
-
-items.forEach(item => item.addEventListener('click', toggleAccordion));
